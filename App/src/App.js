44f4bb56bc61917c3d05f0cb913b83e113fdb844
Govindakumar181 <Govindakumar181@gmail.com>
2021-02-15 23:40:24 +0500

    Counter, Day/Night CSS Events added

--- App/src/App.js
+++ App/src/App.js
@@ -1,19 +1,23 @@
 import React,{useState} from 'react';
+import Msg from './Message.js';
 import './App.css';
 
 function App() {
   let [count,setCount]=useState(1)
+  let [isMorning,setMorning]=useState(true)
   return(
-    <div>
+    <div className={isMorning ? 'box' : 'Night' }>
       <h1>Hello World</h1>
-      <h3>the value of count: {count}</h3>
+      <h3>Time = { isMorning ? 'Morning' : 'Night' }</h3>
+      <Msg counter={count} />
       <button onClick={
         ()=>setCount(count+1)
-
       }>Update Count</button>
-
+      <button onClick={
+        ()=>setMorning(!isMorning)
+      }>Update Day</button>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ App/src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Msg(props) {
+    return(
+        <h3>The value of Count: {props.counter} </h3>
+    )
+    
+}
+
+export default Msg;
