1e474129f28cdfd869a8d373313731500800b611
Govindakumar181 <Govindakumar181@gmail.com>
2021-02-15 21:43:34 +0500

    setState count value

--- App/src/App.js
+++ App/src/App.js
@@ -1,14 +1,19 @@
-
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>Hello World</h1>
-      </div>
+  let [count,setCount]=useState(1)
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <h3>the value of count: {count}</h3>
+      <button onClick={
+        ()=>setCount(count+1)
+
+      }>Update Count</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
